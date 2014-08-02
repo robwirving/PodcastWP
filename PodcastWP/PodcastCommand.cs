cd5f468ddb4b00bf35a7dd5ee11938496e918b40
robwirving <rob.irving@gmail.com>
2014-08-02 16:09:05 -0400

    Support for Universal Apps
    Seperated into two seperate projects. One library for Windows Phone 8.0
    and one for Windows Phone Universal style Apps. Mostly shared code
    between the two, but necessary to create separate dlls

---
+++ PodcastWP/PodcastCommand.cs
@@ -0,0 +1,25 @@
+﻿namespace PodcastWP
+{
+    public enum PodcastCommand
+    {
+        Launch,
+        Play,
+        Pause,
+        SkipNext,
+        SkipPrevious,
+        Subscribe
+    }
+
+    public enum PlayMode
+    {
+        None,
+        Recent,
+        Random
+    }
+
+    public enum UiMode
+    {
+        Standard,
+        Car
+    }
+}
---
+++ PodcastWP/PodcastHelper.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using PodcastWP.Extensions;
+
+namespace PodcastWP
+{
+    public static class PodcastHelper
+    {
+        private const string PodcastScheme = "wp-podcast://";
+
+        private const string PlayModeArgument = "playMode";
+        private const string UiModeArgument = "uiMode";
+        private const string CallbackUriArgument = "callbackUri";
+        private const string CallbackNameArgument = "callbackName";
+        private const string FeedUrlArgument = "feedUrl";
+
+        /// <summary>
+        /// Launches a podcast app w/ a specified command
+        /// </summary>
+        /// <param name="command">The command.</param>
+        /// <param name="playMode">The mode of playback</param>
+        /// <param name="uiMode">The mode of the UI</param>
+        /// <param name="callbackUri">The callback URI for your app if you want to be called back after the podcast app finishes its command.</param>
+        /// <param name="callbackName">The name of your app which could be displayed in the target podcast app</param>
+        public static async void CommandPodcastApp(PodcastCommand command, PlayMode playMode = PlayMode.None, UiMode uiMode = UiMode.Standard, string callbackUri = "", string callbackName = "")
+        {
+            var uri = string.Format("{0}{1}/", PodcastScheme, command.ToString());
+
+            var queryParams = new List<string>();
+            if (playMode != PlayMode.None)
+                queryParams.Add(string.Format("{1}={0}", playMode, PlayModeArgument));
+            if (uiMode != UiMode.Standard)
+                queryParams.Add(string.Format("{1}={0}", uiMode, UiModeArgument));
+            if (!string.IsNullOrEmpty(callbackUri))
+                queryParams.Add(string.Format("{1}={0}", callbackUri, CallbackUriArgument));
+            if (!string.IsNullOrEmpty(callbackName))
+                queryParams.Add(string.Format("{1}={0}", callbackName, CallbackNameArgument));
+
+            var queryString = string.Join("&", queryParams);
+
+            uri += (!string.IsNullOrEmpty(queryString) ? string.Format("?{0}", queryString) : string.Empty);
+
+            await Windows.System.Launcher.LaunchUriAsync(new Uri(uri));
+        }
+
+        /// <summary>
+        /// Launches a podcast app w/ a request to subscribe to a specified podcast
+        /// </summary>
+        /// <param name="feedUrl">Url for the podcast feed</param>
+        /// <param name="callbackUri">The callback URI for your app if you want to be called back after the podcast app finishes its command.</param>
+        /// <param name="callbackName">The name of your app which could be displayed in the target podcast app</param>
+        public static async void SubscribeToPodcast(Uri feedUrl, string callbackUri = "", string callbackName = "")
+        {
+            var uri = string.Format("{0}{1}/", PodcastScheme, PodcastCommand.Subscribe.ToString());
+
+            var queryParams = new List<string>();
+            
+            queryParams.Add(string.Format("{1}={0}", feedUrl.OriginalString, FeedUrlArgument));
+            if (!string.IsNullOrEmpty(callbackUri))
+                queryParams.Add(string.Format("{1}={0}", callbackUri, CallbackUriArgument));
+            if (!string.IsNullOrEmpty(callbackName))
+                queryParams.Add(string.Format("{1}={0}", callbackName, CallbackNameArgument));
+
+            var queryString = string.Join("&", queryParams);
+
+            uri += (!string.IsNullOrEmpty(queryString) ? string.Format("?{0}", queryString) : string.Empty);
+
+            await Windows.System.Launcher.LaunchUriAsync(new Uri(uri));
+        }
+
+        /// <summary>
+        /// Determines whether the specified URI uses the podcast URI scheme
+        /// </summary>
+        /// <param name="uri">The URI.</param>
+        /// <returns>True if pocket data is present</returns>
+        public static bool HasPodcastUri(Uri uri)
+        {
+            if (uri.ToString().Contains(PodcastScheme))
+                return true;
+            else
+            {
+                var escapedProtocol = Uri.EscapeDataString(PodcastScheme);
+                return uri.ToString().Contains(escapedProtocol);
+            }
+        }
+        
+        /// <summary>
+        /// Retrieves the podcast action.
+        /// </summary>
+        /// <param name="uri">The URI.</param>
+        /// <returns>The deserialised podcast action</returns>
+        public static PodcastAction RetrievePodcastAction(Uri uri)
+        {
+            string podcastUri = string.Empty;
+            if (uri.ToString().Contains(PodcastScheme))
+                podcastUri = uri.ToString();
+            else
+            {
+                podcastUri = uri.ToString().Replace("/Protocol?encodedLaunchUri=", string.Empty);
+                podcastUri = Uri.UnescapeDataString(podcastUri);
+            }
+
+            var commandString = new Uri(podcastUri, UriKind.Absolute).CommandString();
+            var queryString = new Uri(podcastUri, UriKind.Absolute).QueryString();
+
+            PodcastCommand command = (PodcastCommand)Enum.Parse(typeof(PodcastCommand), commandString, true);
+
+            var playMode = queryString.ContainsKey(PlayModeArgument) ? (PlayMode)Enum.Parse(typeof(PlayMode), queryString[PlayModeArgument], true) : PlayMode.None;
+            var uiMode = queryString.ContainsKey(UiModeArgument) ? (UiMode)Enum.Parse(typeof(UiMode), queryString[UiModeArgument], true) : UiMode.Standard;
+            var callbackUri = queryString.ContainsKey(CallbackUriArgument) ? queryString[CallbackUriArgument] : string.Empty;
+            var callbackName = queryString.ContainsKey(CallbackNameArgument) ? queryString[CallbackNameArgument] : string.Empty;
+            var feedUrl = queryString.ContainsKey(FeedUrlArgument) ? new Uri(queryString[FeedUrlArgument], UriKind.RelativeOrAbsolute) : null;
+
+            PodcastAction action = new PodcastAction
+            {
+                Command = command,
+                PlayMode = playMode,
+                UiMode = uiMode,
+                FeedUrl = feedUrl,
+                CallbackUri = callbackUri,
+                CallbackName = callbackName
+            };
+
+            return action;
+        }
+    }
+}
